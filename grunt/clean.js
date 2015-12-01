ee1b8c436be98dcbf6a777357e1c8f67c6728691
jrs1 <jsillitoe@gmail.com>
2015-12-01 15:41:00 -0800

    Added grunt support

---
+++ grunt/clean.js
@@ -0,0 +1,13 @@
+/**
+ * Clean files and folders.
+ *
+ * https://www.npmjs.org/package/grunt-contrib-clean
+ */
+
+module.exports = {
+
+    build: ["build/*"],  // clean all the files from the build directory
+
+    dist: ["dist/*"] // clean all the files from the distribution directory
+
+};
